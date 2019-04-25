b62f1cb243022b26d6dc15fb608832a90b3627dc
L Shaf <shafry@mimicreative.net>
2019-04-25 12:12:28 +0700

    🎰 just adding output

--- src/Command/ProcessCommand.php
+++ src/Command/ProcessCommand.php
@@ -26,6 +26,7 @@ class ProcessCommand extends ContainerAwareCommand
     
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $output->writeln(" [#] Listening your service");
         $this->amqp->process($this->logger);
     }
 }
